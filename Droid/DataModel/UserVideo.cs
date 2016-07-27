a1bab1ee634c52bbe69165898d9931e9e3d273c7
Jonathan Fraimorice <yonifra@gmail.com>
2016-07-27 08:29:54 +0300

    Added SQLiteHelper class to interact with DB

---
+++ Droid/DataModel/UserVideo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FiveMin.Droid.DataModel
+{
+    public class UserVideo
+    {
+        public UserVideo ()
+        {
+        }
+
+        /// <summary>
+        /// The Firebase key
+        /// </summary>
+        /// <value>Identifies the video by its key</value>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this <see cref="T:FiveMin.Droid.DataModel.FavVideoDataModel"/>
+        /// is a favorite video.
+        /// </summary>
+        /// <value><c>true</c> if is favorite; otherwise, <c>false</c>.</value>
+        public bool IsFavorite { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this <see cref="T:FiveMin.Droid.DataModel.FavVideoDataModel"/>
+        /// is in the user's watchlist.
+        /// </summary>
+        /// <value><c>true</c> if is in watchlist; otherwise, <c>false</c>.</value>
+        public bool IsInWatchlist { get; set; }
+    }
+}
+
---
+++ Droid/Helpers/SQLiteHelper.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Android.Util;
+using FiveMin.Droid.DataModel;
+using SQLite;
+
+namespace FiveMin.Droid.Helpers
+{
+    public class SQLiteHelper
+    {
+        private readonly string _path;
+        private SQLiteHelper _instance;
+
+        private SQLiteHelper ()
+        {
+            _path = Environment.GetFolderPath (Environment.SpecialFolder.ApplicationData);
+            CreateDatabase ();
+        }
+
+        public SQLiteHelper Instance
+        {
+            get
+            {
+                _instance = _instance ?? new SQLiteHelper ();
+                return _instance;
+            }
+        }
+
+        private async void CreateDatabase ()
+        {
+            try
+            {
+                var connection = new SQLiteAsyncConnection (_path);
+                await connection.CreateTableAsync<UserVideo> ();
+
+            }
+            catch (SQLiteException ex)
+            {
+                Log.Error("SQLiteHelper", ex.Message);
+            }
+        }
+
+        public async Task<string> InsertUpdateDataAsync (UserVideo data)
+        {
+            try
+            {
+                var db = new SQLiteAsyncConnection (_path);
+                if (await db.InsertAsync (data) != 0)
+                    db.UpdateAsync (data);
+                return "Single data file inserted or updated";
+            }
+            catch (SQLiteException ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        public async Task<int> GetNumberOfFavoriteVideosAsync ()
+        {
+            try
+            {
+                var db = new SQLiteAsyncConnection (_path);
+                // this counts all records in the database, it can be slow depending on the size of the database
+                var count = await db.ExecuteScalarAsync<int> ("SELECT Count(*) FROM FavVideoDataModel");
+
+                // for a non-parameterless query
+                // var count = db.ExecuteScalar<int>("SELECT Count(*) FROM Person WHERE FirstName="Amy");
+
+                return count;
+            }
+            catch (SQLiteException ex)
+            {
+                return -1;
+            }
+        }
+
+        public async Task<IEnumerable<string>> GetAllFavoriteVideosAsync ()
+        {
+            try
+            {
+                var db = new SQLiteAsyncConnection (_path);
+                // this counts all records in the database, it can be slow depending on the size of the database
+                var list = await db.ExecuteScalarAsync<IEnumerable<string>> ("SELECT Id FROM UserVideo WHERE IsFavorite = true");
+
+                // for a non-parameterless query
+                // var count = db.ExecuteScalar<int>("SELECT Count(*) FROM Person WHERE FirstName="Amy");
+
+                return list;
+            }
+            catch (SQLiteException ex)
+            {
+                return new List<string>();
+            }
+        }
+
+        public async Task<IEnumerable<string>> GetAllWatchlistVideosAsync ()
+        {
+            try
+            {
+                var db = new SQLiteAsyncConnection (_path);
+                // this counts all records in the database, it can be slow depending on the size of the database
+                var list = await db.ExecuteScalarAsync<IEnumerable<string>> ("SELECT Id FROM UserVideo WHERE IsInWatchlist = true");
+
+                return list;
+            }
+            catch (SQLiteException ex)
+            {
+                return new List<string> ();
+            }
+        }
+    }
+}
+
