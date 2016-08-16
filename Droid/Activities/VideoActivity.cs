9e50f119dcc2d54d6667d655b6310c1f4ba9f104
Jonathan Fraimorice <yonifra@gmail.com>
2016-08-16 23:01:15 +0300

    Fixed video playback

--- Droid/Activities/VideoActivity.cs
+++ Droid/Activities/VideoActivity.cs
@@ -18,9 +18,13 @@ using Google.YouTube.Player;
 namespace FiveMin.Droid.Activities
 {
     [Activity (Label = "Video")]
-    public class VideoActivity : BaseActivity
-    { 
-        protected override int LayoutResource => Resource.Layout.Main;
+    public class VideoActivity : YouTubeFailureRecoveryActivity
+    {
+        private string _videoId;
+        protected override IYouTubePlayerProvider GetYouTubePlayerProvider ()
+        {
+            return FragmentManager.FindFragmentById<YouTubePlayerFragment> (Resource.Id.youtube_fragment);
+        }
 
         protected override void OnCreate (Bundle savedInstanceState)
         {
@@ -28,7 +32,7 @@ namespace FiveMin.Droid.Activities
 
             SetContentView (Resource.Layout.fragment_videoPage);
 
-            var videoId = Intent.GetStringExtra  ("VideoId");
+            _videoId = Intent.GetStringExtra  ("VideoId");
             var videoName = Intent.GetStringExtra  ("VideoName");
             var videoDescription = Intent.GetStringExtra  ("VideoDescription");
             var watchCount = Intent.GetStringExtra  ("WatchCount");
@@ -47,11 +51,22 @@ namespace FiveMin.Droid.Activities
             var progressBar = FindViewById<ProgressBar> (Resource.Id.loadingVideoProgressBar);
             var fab = FindViewById<FloatingActionButton> (Resource.Id.addToFavorite);
 
+            var youTubePlayerFragment = FragmentManager.FindFragmentById<YouTubePlayerFragment> (Resource.Id.youtube_fragment);
+            youTubePlayerFragment.Initialize (DeveloperKey.Key, this);
+
             FontsHelper.ApplyTypeface (Assets, new List<TextView> { videoMetadata, videoNameTextView, descriptionTextView });
 
             progressBar.Visibility = ViewStates.Invisible;
             mainLayout.Visibility = ViewStates.Visible;
             fab.Visibility = ViewStates.Visible;
+        }
+
+        public override void OnInitializationSuccess (IYouTubePlayerProvider provider, IYouTubePlayer player, bool wasRestored)
+        {
+            if (!wasRestored)
+            {
+                player.CueVideo (_videoId);
+            }
         }
     }
 }
